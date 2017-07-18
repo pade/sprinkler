dbb128d0e3546a3b6dbc17792a9e628f0bc93db0
Patrick Dassier <patrick.dassier@legrand.Fr>
2017-07-18 13:34:32 +0200

    xmpp...

--- src/test/test_xmpp.py
+++ src/test/test_xmpp.py
@@ -14,9 +14,9 @@ class TestXMPP(unittest.TestCase):
     """docstring for TestXMPP"""
 
     def setUp(self):
-        self.server = "https://007jabber.com/"
-        self.jid = "sprinkler-test@007jabber.com"
-        self.password = "!s20p21!"
+        self.server = "jabber.hot-chilli.net"
+        self.jid = "pade087@jabber.hot-chilli.net"
+        self.password = "yw5r3aa9"
 
     def tearDown(self):
         pass
@@ -24,6 +24,14 @@ class TestXMPP(unittest.TestCase):
     def test_1(self):
         """ Test XMPP connexion """
         con = xmpp.XMPPData(self.jid, self.password)
-        con.connect()
-        con.process(block=False)
+        con.register_plugin('xep_0030') # Service Discovery
+        con.register_plugin('xep_0004') # Data Forms
+        con.register_plugin('xep_0060') # PubSub
+        con.register_plugin('xep_0199') # XMPP Ping
+        #if con.connect(("server.jabber.hot-chilli.net", 80)):
+        if con.connect(("prout", 80)):
+            con.process(block=True)
+        else:
+            logging.error("Unable to connect")
+            assert False
 
