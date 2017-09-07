af239c0d879d9ac13046847b51725cb9efeacf47
Patrick Dassier <patrick.dassier@legrand.fr>
2017-09-07 17:00:55 +0200

    refactoring test

--- src/test/test_engine.py
+++ src/test/test_engine.py
@@ -1,40 +1,39 @@
 # -*- coding: UTF-8 -*-
 
 from datetime import datetime
-import unittest
 from unittest.mock import MagicMock
-import logging
 import sys
 import os
+import pytest
 
 # Set parent directory in path, to be able to import module
 sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), ".."))
 import engine
-import channel
+from channel import Channel
 import progdays
 import stime
 from hw import gpio
 
 
-class TestEngine(unittest.TestCase):
+class StubHw(gpio.BaseGpio):
 
-    def setUp(self):
-        class StubHw(gpio.BaseGpio):
+    def __init__(self, pconfig):
+        self.cfg = pconfig
+        self.cmd = False
 
-            def __init__(self, pconfig):
-                self.cfg = pconfig
-                self.cmd = False
+    def write(self, pchannel, pvalue):
+        self.cmd = pvalue
 
-            def write(self, pchannel, pvalue):
-                self.cmd = pvalue
+    def read(self, pchannel):
+        return self.cmd
 
-            def read(self, pchannel):
-                return self.cmd
+class ChannelInit():
 
+    def __init__(self):
         self.hw1 = StubHw(None)
         self.hw2 = StubHw(None)
-        self.ch1 = channel.Channel("Channel 1", 0, self.hw1)
-        self.ch2 = channel.Channel("Channel 2", 1, self.hw2)
+        self.ch1 = Channel("Channel 1", 0, self.hw1)
+        self.ch2 = Channel("Channel 2", 1, self.hw2)
 
         self.ch1.progs = [progdays.Progdays(), progdays.Progdays()]
         self.ch2.progs = [progdays.Progdays(), progdays.Progdays()]
@@ -43,255 +42,251 @@ class TestEngine(unittest.TestCase):
         self.ch1.progs[0].stime = stime.STime(hour=5, minute=0, duration=30)
         self.ch1.progs[0].isactive = True
         self.ch1.progs[0].set_days([True, True, True, True, True, True, True])
-        self._logger = logging.getLogger("Test engine")
 
-    def tearDown(self):
-        pass
 
-    def test_1(self):
-        """When channel is not enable, do nothing"""
-        self.ch1.isenable = False
+@pytest.fixture
+def channel():
+    return ChannelInit()
 
-        e = engine.Engine([self.ch1])
-        # Stop scheduler, not used here
-        e.stop()
 
-        e.run()
+def test_1(channel):
+    """When channel is not enable, do nothing"""
+    channel.ch1.isenable = False
 
-        self.assertFalse(self.hw1.cmd)
+    e = engine.Engine([channel.ch1])
+    # Stop scheduler, not used here
+    e.stop()
 
-    def test_2(self):
-        """Activate channel when time is done"""
-        # logger = logging.getLogger()
-        # logger.level = logging.DEBUG
-        # stream_handler = logging.StreamHandler(sys.stdout)
-        # logger.addHandler(stream_handler)
+    e.run()
 
-        e = engine.Engine([self.ch1])
-        # Stop scheduler, not used here
-        e.stop()
+    assert(not channel.hw1.cmd)
 
-        # Mock engine function to force date and time
-        e.get_datetime_now = MagicMock(
-            return_value=datetime(2017, 6, 23, 5, 15))
-        e.run()
-        self.assertTrue(self.hw1.cmd)
+def test_2(channel):
+    e = engine.Engine([channel.ch1])
+    # Stop scheduler, not used here
+    e.stop()
 
-        e.get_datetime_now = MagicMock(
-            return_value=datetime(2017, 6, 23, 5, 31))
-        e.run()
-        self.assertFalse(self.hw1.cmd)
+    # Mock engine function to force date and time
+    e.get_datetime_now = MagicMock(
+        return_value=datetime(2017, 6, 23, 5, 15))
+    e.run()
+    assert(channel.hw1.cmd)
 
-    def test_3(self):
-        """When prog is not active do nothing"""
-        self.ch1.progs[0].isactive = False
+    e.get_datetime_now = MagicMock(
+        return_value=datetime(2017, 6, 23, 5, 31))
+    e.run()
+    assert(not channel.hw1.cmd)
 
-        e = engine.Engine([self.ch1])
-        # Stop scheduler, not used here
-        e.stop()
+def test_3(channel):
+    """When prog is not active do nothing"""
+    channel.ch1.progs[0].isactive = False
 
-        # Mock engine function to force date and time
-        e.get_datetime_now = MagicMock(
-            return_value=datetime(2017, 6, 23, 5, 15))
+    e = engine.Engine([channel.ch1])
+    # Stop scheduler, not used here
+    e.stop()
 
-        e.run()
-        self.assertFalse(self.hw1.cmd)
+    # Mock engine function to force date and time
+    e.get_datetime_now = MagicMock(
+        return_value=datetime(2017, 6, 23, 5, 15))
 
-    def test_4(self):
-        """Deactivate channel outside time range"""
-        e = engine.Engine([self.ch1])
-        # Stop scheduler, not used here
-        e.stop()
+    e.run()
+    assert(not channel.hw1.cmd)
 
-        # Mock engine function to force date and time
-        e.get_datetime_now = MagicMock(
-            return_value=datetime(2017, 6, 23, 5, 31))
+def test_4(channel):
+    """Deactivate channel outside time range"""
+    e = engine.Engine([channel.ch1])
+    # Stop scheduler, not used here
+    e.stop()
 
-        e.run()
-        self.assertFalse(self.hw1.cmd)
+    # Mock engine function to force date and time
+    e.get_datetime_now = MagicMock(
+        return_value=datetime(2017, 6, 23, 5, 31))
 
-        # Mock engine function to force date and time
-        e.get_datetime_now = MagicMock(
-            return_value=datetime(2017, 6, 23, 4, 59))
+    e.run()
+    assert(not channel.hw1.cmd)
 
-        e.run()
-        self.assertFalse(self.hw1.cmd)
+    # Mock engine function to force date and time
+    e.get_datetime_now = MagicMock(
+        return_value=datetime(2017, 6, 23, 4, 59))
 
-    def test_5(self):
-        """Deactivate channel when current day is not selected"""
-        # Set Friday off
-        self.ch1.progs[0].set_one_day(4, False)
+    e.run()
+    assert(not channel.hw1.cmd)
 
-        e = engine.Engine([self.ch1])
-        # Stop scheduler, not used here
-        e.stop()
+def test_5(channel):
+    """Deactivate channel when current day is not selected"""
+    # Set Friday off
+    channel.ch1.progs[0].set_one_day(4, False)
 
-        # Mock engine function to force date and time
-        # 23 june 2017 is Friday
-        e.get_datetime_now = MagicMock(
-            return_value=datetime(2017, 6, 23, 5, 15))
+    e = engine.Engine([channel.ch1])
+    # Stop scheduler, not used here
+    e.stop()
 
-        e.run()
-        self.assertFalse(self.hw1.cmd)
+    # Mock engine function to force date and time
+    # 23 june 2017 is Friday
+    e.get_datetime_now = MagicMock(
+        return_value=datetime(2017, 6, 23, 5, 15))
 
-    def test_6(self):
-        """Where some day are off, other days must runs"""
+    e.run()
+    assert(not channel.hw1.cmd)
 
-        self.ch1.progs[0].set_one_day(0, False)
-        self.ch1.progs[0].set_one_day(1, False)
-        self.ch1.progs[0].set_one_day(2, False)
-        self.ch1.progs[0].set_one_day(3, False)
-        self.ch1.progs[0].set_one_day(4, False)
-        self.ch1.progs[0].set_one_day(6, False)
+def test_6(channel):
+    """Where some day are off, other days must runs"""
 
-        e = engine.Engine([self.ch1])
-        # Stop scheduler, not used here
-        e.stop()
+    channel.ch1.progs[0].set_one_day(0, False)
+    channel.ch1.progs[0].set_one_day(1, False)
+    channel.ch1.progs[0].set_one_day(2, False)
+    channel.ch1.progs[0].set_one_day(3, False)
+    channel.ch1.progs[0].set_one_day(4, False)
+    channel.ch1.progs[0].set_one_day(6, False)
 
-        # 24 june 2017 is Saturday
-        e.get_datetime_now = MagicMock(
-            return_value=datetime(2017, 6, 24, 5, 15))
+    e = engine.Engine([channel.ch1])
+    # Stop scheduler, not used here
+    e.stop()
 
-        e.run()
-        self.assertTrue(self.hw1.cmd)
+    # 24 june 2017 is Saturday
+    e.get_datetime_now = MagicMock(
+        return_value=datetime(2017, 6, 24, 5, 15))
 
-    def test_7(self):
-        """ Test when time is around midnight """
+    e.run()
+    assert(channel.hw1.cmd)
 
-        self.ch1.progs[1].stime = stime.STime(hour=23, minute=50, duration=40)
-        self.ch1.progs[1].isactive = True
-        self.ch1.progs[1].set_days([True, True, True, True, True, True, True])
+def test_7(channel):
+    """ Test when time is around midnight """
 
-        e = engine.Engine([self.ch1])
-        # Stop scheduler, not used here
-        e.stop()
+    channel.ch1.progs[1].stime = stime.STime(hour=23, minute=50, duration=40)
+    channel.ch1.progs[1].isactive = True
+    channel.ch1.progs[1].set_days([True, True, True, True, True, True, True])
 
-        # Mock engine function to force date and time
-        e.get_datetime_now = MagicMock(
-            return_value=datetime(2017, 6, 23, 23, 55))
-        e.run()
-        self.assertTrue(self.hw1.cmd)
+    e = engine.Engine([channel.ch1])
+    # Stop scheduler, not used here
+    e.stop()
 
-        # Mock engine function to force date and time
-        e.get_datetime_now = MagicMock(
-            return_value=datetime(2017, 6, 24, 0, 1))
-        e.run()
-        self.assertTrue(self.hw1.cmd)
+    # Mock engine function to force date and time
+    e.get_datetime_now = MagicMock(
+        return_value=datetime(2017, 6, 23, 23, 55))
+    e.run()
+    assert(channel.hw1.cmd)
 
-        # Mock engine function to force date and time
-        e.get_datetime_now = MagicMock(
-            return_value=datetime(2017, 6, 24, 0, 31))
-        e.run()
-        self.assertFalse(self.hw1.cmd)
+    # Mock engine function to force date and time
+    e.get_datetime_now = MagicMock(
+        return_value=datetime(2017, 6, 24, 0, 1))
+    e.run()
+    assert(channel.hw1.cmd)
 
-    def test_2Channels(self):
-        """ Test 2 channels in parallel"""
-        self.ch2.progs = [progdays.Progdays(), progdays.Progdays()]
+    # Mock engine function to force date and time
+    e.get_datetime_now = MagicMock(
+        return_value=datetime(2017, 6, 24, 0, 31))
+    e.run()
+    assert(not channel.hw1.cmd)
 
-        self.ch2.isenable = True
-        self.ch2.progs[0].stime = stime.STime(hour=5, minute=15, duration=30)
-        self.ch2.progs[0].isactive = True
-        self.ch2.progs[0].set_days([True, True, True, True, True, True, True])
+def test_2Channels(channel):
+    """ Test 2 channels in parallel"""
+    channel.ch2.progs = [progdays.Progdays(), progdays.Progdays()]
 
-        e = engine.Engine([self.ch1, self.ch2])
-        # Stop scheduler, not used here
-        e.stop()
+    channel.ch2.isenable = True
+    channel.ch2.progs[0].stime = stime.STime(hour=5, minute=15, duration=30)
+    channel.ch2.progs[0].isactive = True
+    channel.ch2.progs[0].set_days([True, True, True, True, True, True, True])
 
-        # Mock engine function to force date and time
-        e.get_datetime_now = MagicMock(
-            return_value=datetime(2017, 6, 24, 4, 59))
-        e.run()
+    e = engine.Engine([channel.ch1, channel.ch2])
+    # Stop scheduler, not used here
+    e.stop()
 
-        self.assertTrue(not self.hw1.cmd and not self.hw2.cmd)
+    # Mock engine function to force date and time
+    e.get_datetime_now = MagicMock(
+        return_value=datetime(2017, 6, 24, 4, 59))
+    e.run()
 
-        e.get_datetime_now = MagicMock(
-            return_value=datetime(2017, 6, 24, 5, 10))
-        e.run()
+    assert(not channel.hw1.cmd and not channel.hw2.cmd)
 
-        self.assertTrue(self.hw1.cmd and not self.hw2.cmd)
+    e.get_datetime_now = MagicMock(
+        return_value=datetime(2017, 6, 24, 5, 10))
+    e.run()
 
-        e.get_datetime_now = MagicMock(
-            return_value=datetime(2017, 6, 24, 5, 20))
-        e.run()
+    assert(channel.hw1.cmd and not channel.hw2.cmd)
 
-        self.assertTrue(self.hw1.cmd and self.hw2.cmd)
+    e.get_datetime_now = MagicMock(
+        return_value=datetime(2017, 6, 24, 5, 20))
+    e.run()
 
-        e.get_datetime_now = MagicMock(
-            return_value=datetime(2017, 6, 24, 5, 31))
-        e.run()
+    assert(channel.hw1.cmd and channel.hw2.cmd)
 
-        self.assertTrue(not self.hw1.cmd and self.hw2.cmd)
+    e.get_datetime_now = MagicMock(
+        return_value=datetime(2017, 6, 24, 5, 31))
+    e.run()
 
-    def test_forcedOff(self):
-        """ Forced OFF when  running """
+    assert(not channel.hw1.cmd and channel.hw2.cmd)
 
-        e = engine.Engine([self.ch1])
-        # Stop scheduler, not used here
-        e.stop()
+def test_forcedOff(channel):
+    """ Forced OFF when  running """
 
-        # Force time into a running period
-        e.get_datetime_now = MagicMock(
-            return_value=datetime(2017, 6, 23, 5, 15))
-        e.run()
-        self.assertTrue(self.hw1.cmd)
-        self.ch1.manual = "OFF"
-        e.run()
-        self.assertFalse(self.hw1.cmd)
-        self.ch1.manual = "AUTO"
-        e.run()
-        self.assertTrue(self.hw1.cmd)
+    e = engine.Engine([channel.ch1])
+    # Stop scheduler, not used here
+    e.stop()
 
-    def test_forcedOn(self):
-        """ Forced ON when not running """
+    # Force time into a running period
+    e.get_datetime_now = MagicMock(
+        return_value=datetime(2017, 6, 23, 5, 15))
+    e.run()
+    assert(channel.hw1.cmd)
+    channel.ch1.manual = "OFF"
+    e.run()
+    assert(not channel.hw1.cmd)
+    channel.ch1.manual = "AUTO"
+    e.run()
+    assert(channel.hw1.cmd)
 
-        e = engine.Engine([self.ch1])
-        # Stop scheduler, not used here
-        e.stop()
+def test_forcedOn(channel):
+    """ Forced ON when not running """
 
-        # Force time outside a running period
-        e.get_datetime_now = MagicMock(
-            return_value=datetime(2017, 6, 23, 5, 45))
-        e.run()
-        self.assertFalse(self.hw1.cmd)
-        self.ch1.manual = "ON"
-        e.run()
-        self.assertTrue(self.hw1.cmd)
-        self.ch1.manual = "AUTO"
-        e.run()
-        self.assertFalse(self.hw1.cmd)
+    e = engine.Engine([channel.ch1])
+    # Stop scheduler, not used here
+    e.stop()
 
-    def test_forcedOn_2(self):
-        """ Forced ON when not running using Engine class """
-        e = engine.Engine([self.ch1])
-        # Stop scheduler, not used here
-        e.stop()
+    # Force time outside a running period
+    e.get_datetime_now = MagicMock(
+        return_value=datetime(2017, 6, 23, 5, 45))
+    e.run()
+    assert(not channel.hw1.cmd)
+    channel.ch1.manual = "ON"
+    e.run()
+    assert(channel.hw1.cmd)
+    channel.ch1.manual = "AUTO"
+    e.run()
+    assert(not channel.hw1.cmd)
 
-        # Force time outside a running period
-        e.get_datetime_now = MagicMock(
-            return_value=datetime(2017, 6, 23, 5, 45))
-        e.run()
-        self.assertFalse(self.hw1.cmd)
+def test_forcedOn_2(channel):
+    """ Forced ON when not running using Engine class """
+    e = engine.Engine([channel.ch1])
+    # Stop scheduler, not used here
+    e.stop()
 
-        e.channel_forced(0, "ON")
-        self.assertTrue(self.hw1.cmd)
+    # Force time outside a running period
+    e.get_datetime_now = MagicMock(
+        return_value=datetime(2017, 6, 23, 5, 45))
+    e.run()
+    assert(not channel.hw1.cmd)
 
-        e.channel_forced(0, "AUTO")
-        self.assertFalse(self.hw1.cmd)
+    e.channel_forced(0, "ON")
+    assert(channel.hw1.cmd)
 
-    def test_forcedOff_2(self):
-        """ Forced OFF when not running using Engine class """
-        e = engine.Engine([self.ch1])
-        # Stop scheduler, not used here
-        e.stop()
+    e.channel_forced(0, "AUTO")
+    assert(not channel.hw1.cmd)
 
-        # Force time into a running period
-        e.get_datetime_now = MagicMock(
-            return_value=datetime(2017, 6, 23, 5, 15))
-        e.run()
-        self.assertTrue(self.hw1.cmd)
+def test_forcedOff_2(channel):
+    """ Forced OFF when not running using Engine class """
+    e = engine.Engine([channel.ch1])
+    # Stop scheduler, not used here
+    e.stop()
 
-        e.channel_forced(0, "OFF")
-        self.assertFalse(self.hw1.cmd)
+    # Force time into a running period
+    e.get_datetime_now = MagicMock(
+        return_value=datetime(2017, 6, 23, 5, 15))
+    e.run()
+    assert(channel.hw1.cmd)
 
-        e.channel_forced(0, "AUTO")
-        self.assertTrue(self.hw1.cmd)
+    e.channel_forced(0, "OFF")
+    assert(not channel.hw1.cmd)
+
+    e.channel_forced(0, "AUTO")
+    assert(channel.hw1.cmd)
