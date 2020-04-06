4102c8c6ca9653d25acee2828747f9ae41b568eb
Patrick Dassier <patrick.dassier@legrand.fr>
2020-04-06 15:51:56 +0200

    Fix issue

--- src/scheduler.py
+++ src/scheduler.py
@@ -22,20 +22,19 @@ class Scheduler(object):
         @param target: function to execute every minute
         @param args: tuple of arguments to pass to target
         '''
-        self._stop = Event()
-        self._sched = Thread(target=self._run, args=(target, args, self._stop))
+        self._stop = False
+        self._sched = Thread(target=self._run, args=(target, args))
         self._sched.start()
         self._logger = logging.getLogger('sprinkler')
 
-    def _run(self, target, args, stop):
+    def _run(self, target, args):
         '''
         Execute target function every minutes
         @param target: function to execute
         @param args: argument to pass to function
-        @param stop: Event object that is set to stop the execution
         '''
         oldtime = datetime.datetime.now()
-        while not stop.is_set():
+        while not self._stop:
             newtime = datetime.datetime.now()
             if(newtime.minute != oldtime.minute):
                 self._logger.debug("Scheduler fires now...")
@@ -44,14 +43,12 @@ class Scheduler(object):
                 else:
                     target(args)
                 oldtime = newtime
-        # stop execution
-        stop.clear()
 
     def stop(self):
         """
         Stop execution
         """
-        self._stop.set()
+        self._stop = True
 
     def is_alive(self):
         """ Return True if target function is schedule every minute
