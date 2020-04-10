f83408919b4fc9ef3a120f1b79ff9b453a48f02a
Patrick Dassier <patrick.dassier@legrand.fr>
2020-04-10 15:05:58 +0200

    Improve timer class and test

--- src/test/test_timer.py
+++ src/test/test_timer.py
@@ -4,17 +4,15 @@ import os
 import sys
 import time
 import pytest
+from datetime import datetime, timedelta
+import logging
 
 # Set parent directory in path, to be able to import module
 sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), ".."))
 import timer
 
-def testSingleton():
-    t1 = timer.Timer()
-    t2 = timer.Timer()
-    assert(id(t1) == id(t2))
 
-def testTimerSecond():
+def test_TimerSecond():
     def action(start):
         t.stop()
         assert(int(time.time())-start == 6.0)
@@ -22,3 +20,40 @@ def testTimerSecond():
     t = timer.Timer()
     t.program(0.1, action, argument=(int(time.time()),))
     t.start()
+
+def test_SeveralTimer():
+    nb_call = 0
+
+    def action():
+        nonlocal nb_call
+        nb_call += 1
+
+    t = timer.Timer()
+    t.program(2.0/60.0, action)
+    t.program(3.0/60.0, action)
+    t.start()
+
+    starttime = datetime.now()
+    while(nb_call != 2 and  datetime.now() < starttime + timedelta(seconds=20)):
+        pass
+    t.stop()
+    assert(nb_call == 2)
+
+def test_ClearTimer():
+    nb_call = 0
+
+    def action():
+        nonlocal nb_call
+        nb_call += 1
+    
+    t = timer.Timer()
+    t.program(1.0/60.0, action)
+    t.program(2.0/60.0, action)
+    t.start()
+    t.clear()
+    starttime = datetime.now()
+    while(datetime.now() < starttime + timedelta(seconds=5)):
+        pass
+    t.stop()
+    assert(nb_call == 0)
+
--- src/timer.py
+++ src/timer.py
@@ -4,17 +4,7 @@ import threading
 
 
 class Timer(threading.Thread):
-    _instance = None
-    _lock = threading.Lock()
-
-    def __new__(cls):
-        """ To create a singleton class """
-        if Timer._instance is None:
-            with Timer._lock:
-                if Timer._instance is None:
-                    Timer._instance = super(Timer, cls).__new__(cls)
-        return Timer._instance
-        
+       
     def __init__(self):
         self._sched= sched.scheduler(time.time, time.sleep)
         self._stoptimer = False
@@ -33,9 +23,9 @@ class Timer(threading.Thread):
             self._sched.cancel(event)
 
     def stop(self):
-        self._stoptimer = True
         for event in self._sched.queue:
             self._sched.cancel(event)
+        self._stoptimer = True
 
     
 
