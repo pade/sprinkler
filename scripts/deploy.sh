3565d604dbc03d838f7a639117ffb807f78cb6de
Patrick Dassier <patrick.dassier@legrand.fr>
2020-04-16 11:55:39 +0200

    Change deployment scirpts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,3 +11,11 @@ fi
 git -C $WORK_DIR checkout $BRANCH
 
 cd $WORK_DIR && pipenv install
+sudo cp $WORK_DIR/scripts/sprinkler.service /etc/systemd/system
+sudo systemctl stop sprinkler.service
+sudo systemctl enable sprinkler.service
+sudo systemctl start sprinkler.service
+sudo systemctl daemon-reload
+
+
+
