a76f4b7ce470240dbb8fbface99cdbf5730581de
Patrick Dassier <patrick.dassier@legrand.fr>
2020-05-04 14:30:43 +0200

    Fix deployment script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,12 +5,13 @@ BRANCH=deploy
 
 if [ ! -d $WORK_DIR ] ; then
     git clone https://github.com/pade/sprinkler.git $WORK_DIR
+    git -C $WORK_DIR checkout $BRANCH
+    cd $WORK_DIR && pipenv install
 else
+    git -C $WORK_DIR checkout $BRANCH
     git -C $WORK_DIR pull
+    cd $WORK_DIR && pipenv sync
 fi
-git -C $WORK_DIR checkout $BRANCH
-
-cd $WORK_DIR && pipenv install
 sudo cp $WORK_DIR/scripts/sprinkler.service /etc/systemd/system
 sudo systemctl stop sprinkler.service
 sudo systemctl enable sprinkler.service
