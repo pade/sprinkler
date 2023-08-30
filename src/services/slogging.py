3c21a656af7c579797b391d1d81a1e1d152ec759
Patrick Dassier <patrick.dassier@legrand.com>
2023-08-30 15:17:30 +0200

    Add custom logs

---
+++ src/services/slogging.py
@@ -0,0 +1,21 @@
+import logging
+import sys
+
+
+class SLogging(logging.getLoggerClass()):
+    
+    def __init__(self, name: str, level: str | int = 0) -> None:
+        super().__init__(name, level)
+        formatter = logging.Formatter(
+            '%(asctime)s - %(name)s [%(levelname)s]: %(message)s'
+        )
+        streamhandler = logging.StreamHandler(sys.stdout)
+        streamhandler.setFormatter(formatter)
+        self.addHandler(streamhandler)
+        
+    def logCall(self, func):
+        def wrapper(*args, **kwargs):
+            self.debug(f'Calling: {func.__name__}({args}, {kwargs})')
+            func(*args, **kwargs)
+        return wrapper
+ 
